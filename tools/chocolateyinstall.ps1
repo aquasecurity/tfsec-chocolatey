0b0441cc3a0d18a7339bdbc792706efd5eb024d6
owenrumney <owen@owenrumney.co.uk>
2020-10-13 22:21:20 +0100

    Adding initial commit
    Initial commit of the chocolatey repo

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName = 'tfsec'
+$checksum = '56efc6f9de666b9f8b87e1af44308b9b9674559a60ec8f6a4ca12abe7b8d284c'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$version = "v0.30.1"
+$url = 'https://github.com/tfsec/tfsec/releases/download/$version/tfsec-windows-amd64.exe'
+$destination = "$toolsPath\tfsec.exe"
+
+Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
