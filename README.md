4a938b452f8bee885ed78e7e9fc92ccf0ba4e89a
Owen Rumney <owen@owenrumney.co.uk>
2021-04-06 14:09:53 +0100

    Update readme and simplify the version updating

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tfsec-chocolatey
 Chocolatey definition files for tfsec
+
+## Updating 
+
+Updating is simple - run the make file with the latest windows version tag; eg for `v0.39.20`
+
+```shell
+VERSION=v0.39.20 make update
+```
+
+Then commit the changes and push the branch, when it hits main it will create the new version using the Github action
---
+++ update.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+VERSION=$1
+
+if [[ -z "${VERSION}" ]]; then
+    echo "you need to provide a version"
+    exit 1
+fi
+
+wget -O /tmp/tfsec-windows-amd64.exe https://github.com/tfsec/tfsec/releases/download/$VERSION/tfsec-windows-amd64.exe 
+
+SHA=`shasum -a 256 /tmp/tfsec-windows-amd64.exe | awk '{print $1}'`
+
+sed  "s/{PLACEHOLDER_VERSION}/${VERSION}/g; s/{PLACEHOLDER_SHA}/${SHA}/g;" templates/chocolateyinstall.ps1.template > tools/chocolateyinstall.ps1
+sed "s/{PLACEHOLDER_VERSION}/${VERSION}/g" templates/tfsec.nuspec.template tfsec.nuspec
