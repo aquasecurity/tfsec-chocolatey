5511d0b8e1c9a8a990e37466a782f44f9916805f
Owen Rumney <owen@owenrumney.co.uk>
2021-04-21 09:10:54 +0100

    update to the latest version of tfsec

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'tfsec'
-$checksum = '372df63f75fb96e51ad14a5c0b4388b8492551a9eef6588267919d8aab5ecfea'
+$checksum = '4aca249c738186b1f5970e71a4aac8ceb9d8fad7cad1a587eed49e2f392c2e2a'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$version = 'v0.39.21'
+$version = 'v0.39.22'
 $url = "https://github.com/tfsec/tfsec/releases/download/$version/tfsec-windows-amd64.exe"
 $destination = "$toolsPath\tfsec.exe"
 
