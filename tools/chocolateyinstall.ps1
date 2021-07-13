bc946d72b3e6b515ec7afcea9133750f9e7e0f29
Owen Rumney <owen.rumney@aquasec.com>
2021-07-13 14:00:44 +0100

    Update the nuget file to support aquasecurity

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'tfsec'
-$checksum = '630f21a9da76a05aefec72358f61febe70cb25a2045eb80f0e3db0a4a9bde71b'
+$checksum = 'e4267390ac506853636c831f568e599188f3943307411a060195f85309d1926a'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$version = 'v0.45.3'
-$url = "https://github.com/tfsec/tfsec/releases/download/$version/tfsec-windows-amd64.exe"
+$version = 'v0.46.0'
+$url = "https://github.com/aquasecurity/tfsec/releases/download/$version/tfsec-windows-amd64.exe"
 $destination = "$toolsPath\tfsec.exe"
 
 Get-ChocolateyWebFile -PackageName $packageName -FileFullPath $destination -Url64bit $url -Checksum64 $checksum -ChecksumType64 sha256
