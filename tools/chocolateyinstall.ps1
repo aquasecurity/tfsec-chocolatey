daf04db88b518340ea7c60f6351e59779897ad2a
Owen Rumney <owen.rumney@aquasec.com>
2022-03-29 12:13:12 +0100

    chore: update the latest version to v1.15.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = 'Stop'
 
 $packageName = 'tfsec'
-$checksum = 'a26be39ae1dcd8700907b05de1613c6e20f4757bc91c2603a09522ae16182c22'
+$checksum = '33be534d04317a9b3610ea336e0e513c1d5ed4ad9f290f2055b875626f1d1a3b'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$version = 'v1.8.0'
+$version = 'v1.15.0'
 $url = "https://github.com/aquasecurity/tfsec/releases/download/$version/tfsec-windows-amd64.exe"
 $destination = "$toolsPath\tfsec.exe"
 
